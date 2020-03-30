8d49c4b6d20201dfac0d4804d5dc9d3cef260eec
文野 <zhang_colin@163.com>
2020-03-30 10:27:03 +0800

    通过actuator和应用启动时记录的pid，优雅关闭应用

--- ekin-server/src/main/java/com/ekin/EkinApplication.java
+++ ekin-server/src/main/java/com/ekin/EkinApplication.java
@@ -1,28 +1,39 @@
 package com.ekin;
 
 import com.cartisan.utils.SnowflakeIdWorker;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PreDestroy;
 
 /**
  * @author colin
  */
 @SpringBootApplication(scanBasePackages = {"com.cartisan", "com.ekin"})
 @MapperScan("com.ekin.**.mapper")
+@Slf4j
 public class EkinApplication {
     public static void main(String[] args) {
-        //'cat /Users/huangqingshi/app.pid | xargs kill' */
-//        SpringApplication application = new SpringApplication(EkinApplication.class);
-//        application.addListeners(new ApplicationPidFileWriter("/Users/huangqingshi/app.pid"));
-//        application.run();
+        /* 在指定的目录下生成应用pid，执行下面的命令关闭应用 :
+            'cat /Users/colin/app.pid | xargs kill' */
+        SpringApplication application = new SpringApplication(EkinApplication.class);
+        application.addListeners(new ApplicationPidFileWriter("/Users/colin/ekin.pid"));
+        application.run(args);
 
-        SpringApplication.run(EkinApplication.class, args);
+//        SpringApplication.run(EkinApplication.class, args);
     }
 
     @Bean
     public SnowflakeIdWorker idWorker() {
         return new SnowflakeIdWorker(1, 1);
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.debug("Ekin application shutdown");
     }
 }
